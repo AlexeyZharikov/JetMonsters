6f319f69cee6c9df6dcc5dc19ed3b413c19d366a
AlexeyZharikov <mefis2.0@gmail.com>
2022-10-07 12:05:37 +0300

    update 4

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import './pagination.scss'
+import './pagination.scss';
 
 const Pagination = ({imagesPerPage, totalImages, paginate}) => {
   const pageNumbersArr = [];
   for (let i = 1; i <= (totalImages / imagesPerPage); i++) {
     pageNumbersArr.push(i)
   }
-  const pageNumbers = pageNumbersArr.map(item => <div className='page-number' key={item}>
-    <a href="!#" className="page-linl" onClick={() => paginate(item)}>{item}</a>
+  const pageNumbers = pageNumbersArr.map(item => 
+  <div className='page-number' key={item}>
+    <a htrf="/" className="page-link" onClick={() => paginate(item)}>{item}</a>
   </div>)
   return (
     <div className="pagination">
